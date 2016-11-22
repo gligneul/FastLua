225e267f4309535bbb1e53a09e7c8948fb93abce
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2016-11-21 23:03:57 -0200

    Added debuging function for ir

--- src/fl_ir.c
+++ src/fl_ir.c
@@ -23,6 +23,7 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
 
 #include "lprefix.h"
 
@@ -73,43 +74,43 @@ IRBBlock *IRcreatebb(IRFunction *F) {
 
 IRValue* IRconst_c(IRFunction *F, char k) {
   IRValue *v = createvalue(F, IR_CHAR, IR_CONST);
-  v->args.konst.u.c = k;
+  v->args.konst.c = k;
   return v;
 }
 
 IRValue* IRconst_s(IRFunction *F, short k) {
   IRValue *v = createvalue(F, IR_SHORT, IR_CONST);
-  v->args.konst.u.s = k;
+  v->args.konst.s = k;
   return v;
 }
 
 IRValue* IRconst_i(IRFunction *F, int k) {
   IRValue *v = createvalue(F, IR_INT, IR_CONST);
-  v->args.konst.u.i = k;
+  v->args.konst.i = k;
   return v;
 }
 
 IRValue* IRconst_li(IRFunction *F, lua_Integer k) {
   IRValue *v = createvalue(F, IR_LUAINT, IR_CONST);
-  v->args.konst.u.li = k;
+  v->args.konst.li = k;
   return v;
 }
 
 IRValue* IRconst_lf(IRFunction *F, lua_Number k) {
   IRValue *v = createvalue(F, IR_LUAFLT, IR_CONST);
-  v->args.konst.u.lf = k;
+  v->args.konst.lf = k;
   return v;
 }
 
 IRValue* IRconst_iptr(IRFunction *F, l_mem k) {
   IRValue *v = createvalue(F, IR_INTPTR, IR_CONST);
-  v->args.konst.u.iptr = k;
+  v->args.konst.iptr = k;
   return v;
 }
 
 IRValue* IRconst_p(IRFunction *F, void *k) {
   IRValue *v = createvalue(F, IR_PTR, IR_CONST);
-  v->args.konst.u.p = k;
+  v->args.konst.p = k;
   return v;
 }
 
@@ -141,4 +142,88 @@ IRValue* IRbinop(IRFunction *F, int op, IRValue *l, IRValue *r) {
   v->args.binop.r = r;
   return v;
 }
+
+/*
+ * Printing functions for debug
+ */
+
+static void printtype(int type) {
+  switch (type) {
+    case IR_CHAR:   printf("char"); break;
+    case IR_SHORT:  printf("short"); break;
+    case IR_INT:    printf("int"); break;
+    case IR_LUAINT: printf("luaint"); break;
+    case IR_INTPTR: printf("intptr"); break;
+    case IR_LUAFLT: printf("luafloat"); break;
+    case IR_PTR:    printf("ptr"); break;
+    default: assert(0); break;
+  }
+}
+
+static void printconst(int type, union IRConstant k) {
+  switch (type) {
+    case IR_CHAR:   printf("%d", k.c); break;
+    case IR_SHORT:  printf("%d", k.s); break;
+    case IR_INT:    printf("%d", k.i); break;
+    case IR_LUAINT: printf("%lli", k.li); break;
+    case IR_INTPTR: printf("%li", k.iptr); break;
+    case IR_LUAFLT: printf("%f", k.lf); break;
+    case IR_PTR:    printf("%p", k.p); break;
+    default: assert(0); break;
+  }
+}
+
+static void printbinop(int cmd) {
+  switch (cmd) {
+    case IR_ADD: printf("add"); break;
+    case IR_SUB: printf("sub"); break;
+    case IR_MUL: printf("mul"); break;
+    case IR_DIV: printf("div"); break;
+    default: assert(0); break;
+  }
+}
+
+static void printvalue(IRValue *v) {
+  printf("v%d : ", v->id);
+  printtype(v->type);
+  printf(" = ");
+  switch (v->cmd) {
+    case IR_CONST:
+      printf("const ");
+      printconst(v->type, v->args.konst);
+      break;
+    case IR_GETARG:
+      printf("getarg %d", v->args.getarg.n);
+      break;
+    case IR_LOAD:
+      printf("load v%d", v->args.load.mem->id);
+      break;
+    case IR_STORE:
+      printf("store v%d <- v%d", v->args.store.mem->id, v->args.store.v->id);
+      break;
+    case IR_ADD:
+    case IR_SUB:
+    case IR_MUL:
+    case IR_DIV:
+      printbinop(v->cmd);
+      printf(" v%d v%d", v->args.binop.l->id, v->args.binop.r->id);
+      break;
+  }
+  printf("\n");
+}
+
+void IRprint(IRFunction *F) {
+  IRBBlock *bb;
+  IRValue *v;
+  printf("IR DEBUG - F (%p)\n", (void *)F);
+  for (bb = F->bbs; bb != F->bbs + F->nbbs; ++bb) {
+    printf("bblock (%p):\n", (void *)bb);
+    for (v = bb->values; v != bb->values + bb->nvalues; ++v) {
+      printvalue(v);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
 
